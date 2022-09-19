3d07156a95ebf954770c109b5eb429be81349993
tabish-nadeem <tabish.nadeem@atmecs.com>
2022-09-19 13:24:09 +0530

    Corrected ScoringModal step file

--- e2e/playwright/suites/suite02-Scoring_Modal/steps/scoringModal.step.ts
+++ e2e/playwright/suites/suite02-Scoring_Modal/steps/scoringModal.step.ts
@@ -4,29 +4,34 @@ import { Utils } from '../../../../common/utils';
 import { GlobalTenantUtils } from '../../../../common/globalTenantUtils';
 import { FeatureToggleUtils } from '../../../../common/FeatureToggleUtils';
 import * as _ from 'lodash';
-import FormDesignerPagePO from '../../../../pageObjects/form-designer-page.po';
-import { FormAreaComponentPo } from '../../../../pageObjects/form-area.component.po';
-import { DesignerToolbarComponentPO } from '../../../../pageObjects/designer-toolbar.component.po';
-import { ScoringModalComponentPo } from '../../../../pageObjects/scoring-modal.component.po';
-import { ElementAttributesComponentPo } from '../../../../pageObjects/element-attributes.component.po';
-import { ManageFormsPO } from '../../../../pageObjects/manage-forms.po';
+import FormDesignerPagePO from '../../../../pageObjects/FormDesignerPagePO';
+import { FormAreaComponentPo } from '../../../../pageObjects/FormAreaComponentPO';
+import { DesignerToolbarComponentPO } from '../../../../pageObjects/DesignerToolbarComponentPO';
+import { ScoringModalComponentPo } from '../../../../pageObjects/ScoringModalComponentPO';
+import { ElementAttributesComponentPo } from '../../../../pageObjects/ElementAttributesComponentPO';
+import { ManageFormsPO } from '../../../../pageObjects/ManageFormsPO';
 import { LoginPage } from '../../../../common/login';
-import { FEATURE_TOGGLES } from '../../../../common/uiConstants';
 let _ = require('lodash');
-let browser: any;
-let context: BrowserContext;
-let page: Page;
-let formDesignerPage;
-let formArea;
-let designerToolbar;
-let scoringModal;
-let elementAttributes;
-let manageFormsPO;
-let loginPage:LoginPage;
-let userToken: any ;
-let userDetails ;
-let globalTenantUtils:GlobalTenantUtils;
 
+let browser: any,
+    context: BrowserContext,
+    page: Page,
+    formDesignerPage:FormDesignerPagePO,
+    formArea:FormAreaComponentPo,
+    designerToolbar:DesignerToolbarComponentPO,
+    scoringModal:ScoringModalComponentPo,
+    elementAttributes:ElementAttributesComponentPo,
+    manageFormsPO:ManageFormsPO,
+    loginPage:LoginPage,
+    userToken: any ,
+    userDetails:any ,
+    globalTenantUtils:GlobalTenantUtils;
+
+
+const FEATURE_TOGGLES = {
+    navigation_redesign: 'release-navigation-redesign-CXCROSS-21',
+    angular8_migration_spring20: 'utility-QM-angular8Upgrade-CXQM-12535',
+};
 
 BeforeAll({ timeout: 300 * 1000 }, async () => {
     browser = await chromium.launch({
@@ -37,56 +42,55 @@ BeforeAll({ timeout: 300 * 1000 }, async () => {
     page = await context.newPage();
     userDetails = globalTenantUtils.getDefaultTenantCredentials();
     formDesignerPage = new FormDesignerPagePO();
-    formArea = new FormAreaComponentPo();
-    designerToolbar = new DesignerToolbarComponentPO();
+    formArea = new FormAreaComponentPo(page);
+    designerToolbar = new DesignerToolbarComponentPO(page);
     scoringModal = new ScoringModalComponentPo();
-    elementAttributes = new ElementAttributesComponentPo();
+    elementAttributes = new ElementAttributesComponentPo(page);
     manageFormsPO = new ManageFormsPO(page);
     loginPage = new LoginPage(page);
     userToken = await loginPage.login(userDetails.adminCreds.email, userDetails.adminCreds.password);
-    await FeatureToggleUtils.addTenantToFeature(FEATURE_TOGGLES.ANGULAR8_MIGRATION_SPRING20, userDetails.orgName, userToken)
-    await FeatureToggleUtils.addTenantToFeature(FEATURE_TOGGLES.RELEASE_NAVIGATION_REDESIGN, userDetails.orgName, userToken)
-    await manageFormsPO.navigateTo();    
-    await formDesignerPage.navigateTo();
+    await FeatureToggleUtils.addTenantToFeature(FEATURE_TOGGLES.angular8_migration_spring20, userDetails.orgName, userToken)
+    await FeatureToggleUtils.addTenantToFeature(FEATURE_TOGGLES.navigation_redesign, userDetails.orgName, userToken)
+    await manageFormsPO.navigate();    
+    await formDesignerPage.navigate();
     await page.waitForSelector(await formArea.getFormArea());
 });
 
 AfterAll({ timeout: 400 * 1000}, async () =>{
-    await FeatureToggleUtils.removeTenantFromFeature(FEATURE_TOGGLES.ANGULAR8_MIGRATION_SPRING20, userDetails.orgName, userToken);
+    await FeatureToggleUtils.removeTenantFromFeature(FEATURE_TOGGLES.angular8_migration_spring20, userDetails.orgName, userToken);
     await loginPage.logout();
 })
 
 
 
-Given("Step 1: Scoring modal should open after clicking on scoring button and Should close after clicking on set/cancel button",{timeout: 60 * 1000 }, async () => {
+Given("Scoring modal should open after clicking on scoring button and Should close after clicking on set/cancel button",{timeout: 60 * 1000 }, async () => {
     await formArea.dragElementToFormArea('section');
     await formArea.dragElementToFormArea('text');
     await formArea.dragElementToFormArea('radio');
     await formArea.dragElementToSection('radio', '1. Set Title');
     await designerToolbar.clickOnScoringButton();
-    
-    expect(await Utils.isEnabled(scoringModal.getScoringModal())).toBe(true);
+    expect(await page.isEnabled(scoringModal.getScoringModal())).toBe(true);
     await scoringModal.clickCancelModalButton(false, true);
-    expect(await Utils.isPresent(await scoringModal.getSaveButton())).toBe(false);
-    expect(await Utils.isPresent(await scoringModal.getCancelModalButton())).toBe(false);
+    expect(await page.isPresent(await scoringModal.getSaveButton())).toBe(false);
+    expect(await page.isPresent(await scoringModal.getCancelModalButton())).toBe(false);
 });
 
-When("Step-2: Scoring modal should open with default values to the radio button", { timeout: 180 * 1000 }, async () => {
+When("Scoring modal should open with default values to the radio button", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('radio');
     await designerToolbar.clickOnScoringButton();
     expect(await scoringModal.getScoringOfQuestionOption('1. Set question', 0)).toBe('1');
     expect(await scoringModal.getScoringOfQuestionOption('1. Set question', 1)).toBe('1');
-    expect(await Utils.isEnabled(await scoringModal.getSaveButton())).toBe(false);
+    expect(await page.isEnabled(await scoringModal.getSaveButton())).toBe(false);
     await scoringModal.clickCancelModalButton(false, true);
     await formArea.dragElementToFormArea('checkbox');
     await designerToolbar.clickOnScoringButton();
     expect(await scoringModal.getScoringOfQuestionOption('2. Set question', 0)).toBe('1');
     expect(await scoringModal.getScoringOfQuestionOption('2. Set question', 1)).toBe('1');
-    expect(await Utils.isEnabled(await scoringModal.getSaveButton())).toBe(false);
-    expect(await Utils.isEnabled(await scoringModal.getCancelModalButton())).toBe(true);
+    expect(await page.isEnabled(await scoringModal.getSaveButton())).toBe(false);
+    expect(await page.isEnabled(await scoringModal.getCancelModalButton())).toBe(true);
 });
 
-Then("STEP-3: Scoring modal should open with default values to the checkbox after adding multiple options", { timeout: 180 * 1000 }, async () => {
+Then("Scoring modal should open with default values to the checkbox after adding multiple options", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('checkbox');
     await designerToolbar.clickOnScoringButton();
     expect(await scoringModal.getMaxPointsOfQuestion('1. Set question')).toEqual('Max Points:2');
@@ -99,7 +103,7 @@ Then("STEP-3: Scoring modal should open with default values to the checkbox afte
     await elementAttributes.addMultipleModal.clickAddButton();
     await designerToolbar.clickOnScoringButton();
     expect(await scoringModal.getMaxPointsOfQuestion('1. Set question')).toEqual('Max Points:5');
-    expect(await Utils.isEnabled(await scoringModal.getSaveButton())).toBe(false);
+    expect(await page.isEnabled(await scoringModal.getSaveButton())).toBe(false);
     await scoringModal.clickCancelModalButton(false, true);
     await formArea.dragElementToFormArea('radio');
     await elementAttributes.clickAddMultipleButton();
@@ -110,18 +114,18 @@ Then("STEP-3: Scoring modal should open with default values to the checkbox afte
     await designerToolbar.clickOnScoringButton();
     expect(await scoringModal.getMaxPointsOfQuestion('2. Set question')).toEqual('Max Points:1');
 });
-Then("STEP-4: Scoring modal should open with default values to the radio button after adding option using copy", { timeout: 180 * 1000 }, async () => {
+Then("Scoring modal should open with default values to the radio button after adding option using copy", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('radio');
     await designerToolbar.clickOnScoringButton();
     expect(await scoringModal.getMaxPointsOfQuestion('1. Set question')).toEqual('Max Points:1');
-    expect(await Utils.isEnabled(await scoringModal.getSaveButton())).toBe(false);
+    expect(await page.isEnabled(await scoringModal.getSaveButton())).toBe(false);
     await scoringModal.clickCancelModalButton(false, true);
     await formArea.clickCopyElementIcon('1. Set question', 'radio');
     await designerToolbar.clickOnScoringButton();
     expect(await scoringModal.getMaxPointsOfQuestion('2. Set question')).toEqual('Max Points:1');
-    expect(await Utils.isEnabled(await scoringModal.getSaveButton())).toBe(false);
+    expect(await page.isEnabled(await scoringModal.getSaveButton())).toBe(false);
 });
-Then("STEP-5: Scoring modal should open with default values to the checkbox after adding multiple options", { timeout: 180 * 1000 }, async () => {
+Then("Scoring modal should open with default values to the checkbox after adding multiple options", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('checkbox');
     await elementAttributes.clickAddMultipleButton();
     await elementAttributes.addMultipleModal.enterChoice('List Value1');
@@ -146,15 +150,15 @@ Then("STEP-5: Scoring modal should open with default values to the checkbox afte
     await scoringModal.clickCancelModalButton(true, true);
 });
 
-When("STEP-6: Reset button should be disabled by default and get enabled when enable scoring checkbox is checked", { timeout: 180 * 1000 }, async () => {
+When("Reset button should be disabled by default and get enabled when enable scoring checkbox is checked", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('radio');
     await designerToolbar.clickOnScoringButton();
-    expect(await Utils.isEnabled(await scoringModal.getResetButton())).toBe(false);
+    expect(await page.isEnabled(await scoringModal.getResetButton())).toBe(false);
     await scoringModal.clickEnableScoring();
-    expect(await Utils.isEnabled(await scoringModal.getResetButton())).toBe(true);
+    expect(await page.isEnabled(await scoringModal.getResetButton())).toBe(true);
 });
 
-Then("STEP-7: Scoring modal should open with default values to the checkbox after adding multiple options", { timeout: 180 * 1000 }, async () => {
+Then("Scoring modal should open with default values to the checkbox after adding multiple options", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('radio');
     await elementAttributes.choicePropertiesElementAttributes.clickChoiceRadioButton(0);
     await formArea.dragElementToFormArea('checkbox');
@@ -185,7 +189,7 @@ Then("STEP-7: Scoring modal should open with default values to the checkbox afte
     expect(await Utils.isSelected(await scoringModal.getScorableCheckboxOfAQuestion('2. Set question'))).toBeTruthy();
 });
 
-When("STEP-8: Should verify current score is 0 if scoring is not enable", { timeout: 180 * 1000 }, async () => {
+When("Should verify current score is 0 if scoring is not enable", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('radio');
     await elementAttributes.choicePropertiesElementAttributes.clickChoiceRadioButton(0);
     await formArea.dragElementToFormArea('checkbox');
@@ -209,7 +213,7 @@ When("STEP-8: Should verify current score is 0 if scoring is not enable", { time
     expect(await scoringModal.getCurrentPoints()).toEqual('Current points : 4 of 6');
 });
 
-Then("STEP-9: Should verify current score is 100 if user fill all ans correct", { timeout: 180 * 1000 }, async () => {
+Then("Should verify current score is 100 if user fill all ans correct", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('radio');
     await elementAttributes.choicePropertiesElementAttributes.clickChoiceRadioButton(0);
     await formArea.dragElementToFormArea('checkbox');
@@ -229,7 +233,7 @@ Then("STEP-9: Should verify current score is 100 if user fill all ans correct", 
     expect(await scoringModal.getCalculatedScore()).toEqual('Calculated weighted score : 100');
     expect(await scoringModal.getCurrentPoints()).toEqual('Current points : 6 of 6');    
 });
-Then("STEP-10: Should verify current score is getting updated after changing the ans option", { timeout: 180 * 1000 }, async () => {
+Then("Should verify current score is getting updated after changing the ans option", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('radio');
     await elementAttributes.choicePropertiesElementAttributes.clickChoiceRadioButton(0);
     await formArea.dragElementToFormArea('checkbox');
@@ -264,7 +268,7 @@ Then("STEP-10: Should verify current score is getting updated after changing the
     expect(await scoringModal.getCalculatedScore()).toEqual('Calculated weighted score : 57');
     expect(await scoringModal.getCurrentPoints()).toEqual('Current points : 4 of 7');
 });
-Then("STEP-11: Correct current score and percentage should get calculated for elements", { timeout: 180 * 1000 }, async () => {
+Then("Correct current score and percentage should get calculated for elements", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('radio'); //1. Set question
     await elementAttributes.choicePropertiesElementAttributes.clickChoiceRadioButton(0);
     await formArea.dragElementToFormArea('checkbox'); //2. Set question
@@ -299,7 +303,7 @@ Then("STEP-11: Correct current score and percentage should get calculated for el
     expect(await scoringModal.getCalculatedScore()).toEqual('Calculated weighted score : 0');
     expect(await scoringModal.getCurrentPoints()).toEqual('Current points : 0 of 60');
 });
-When("STEP-12: If there is NA, After selecting NA question should get removed from calculation multiple add- radio", { timeout: 180 * 1000 }, async () => {
+When("If there is NA, After selecting NA question should get removed from calculation multiple add- radio", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('radio');
     await elementAttributes.choicePropertiesElementAttributes.clickNACheckbox();
     await elementAttributes.clickAddMultipleButton();
@@ -348,7 +352,7 @@ When("STEP-12: If there is NA, After selecting NA question should get removed fr
     expect(await scoringModal.getCalculatedScore()).toEqual('Calculated weighted score : 20');
     expect(await scoringModal.getCurrentPoints()).toEqual('Current points : 5 of 25');
 });
-Then("STEP-13: If there is NA, After selecting NA question should get removed from calculation - radio", { timeout: 180 * 1000 }, async () => {
+Then("If there is NA, After selecting NA question should get removed from calculation - radio", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('radio');
     await elementAttributes.choicePropertiesElementAttributes.clickNACheckbox();
     await formArea.dragElementToFormArea('checkbox');
@@ -383,22 +387,22 @@ Then("STEP-13: If there is NA, After selecting NA question should get removed fr
     expect(await scoringModal.getCurrentPoints()).toEqual('Current points : 15 of 65');
 });
 
-Then("STEP-14: Scorable checkbox should be disabled by default and should get enabled if enable scoring checkbox is checked", { timeout: 180 * 1000 }, async () => {
+Then("Scorable checkbox should be disabled by default and should get enabled if enable scoring checkbox is checked", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('checkbox');
     await formArea.dragElementToFormArea('section');
     await formArea.dragElementToSection('radio', '2. Set Title');
     await formArea.dragElementToSection('yesno', '2. Set Title');
     await designerToolbar.clickOnScoringButton();
-    expect(await Utils.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('1. Set question'))).toBeFalsy();
-    expect(await Utils.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('2.1 Set question'))).toBeFalsy();
-    expect(await Utils.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('2.2 Set question'))).toBeFalsy();
+    expect(await page.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('1. Set question'))).toBeFalsy();
+    expect(await page.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('2.1 Set question'))).toBeFalsy();
+    expect(await page.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('2.2 Set question'))).toBeFalsy();
     await scoringModal.clickEnableScoring();
-    expect(await Utils.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('1. Set question'))).toBeTruthy();
-    expect(await Utils.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('2.1 Set question'))).toBeTruthy();
-    expect(await Utils.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('2.2 Set question'))).toBeTruthy();
+    expect(await page.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('1. Set question'))).toBeTruthy();
+    expect(await page.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('2.1 Set question'))).toBeTruthy();
+    expect(await page.isEnabled(await scoringModal.getScorableCheckboxOfAQuestion('2.2 Set question'))).toBeTruthy();
 });
 
-Then("STEP-15: Count asterisk(*) on scoring modal", { timeout: 180 * 1000 }, async () => {
+Then("Count asterisk(*) on scoring modal", { timeout: 180 * 1000 }, async () => {
     await formArea.dragElementToFormArea('text');
     await elementAttributes.clickRequiredCheckbox();
     await formArea.dragElementToFormArea('radio');
